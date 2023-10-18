a51bf5b668d35fcd124a1750371e233522271137
jeslinpjames <jeslinpjames007@gmail.com>
2023-10-18 14:17:10 +0530

    Removed the label y and saving each frame in seperate csv file

--- Extract_Features.py
+++ Extract_Features.py
@@ -1,27 +1,25 @@
 import os
 import cv2
 import numpy as np
+import pandas as pd
 from key_frame_extraction import extract_keyframes
 from test_model import extract_features_from_frame
 
-def save_features_and_labels(X, y, output_folder):
-    os.makedirs(output_folder, exist_ok=True)
-    np.save(os.path.join(output_folder, 'X_features.npy'), X)
-    np.save(os.path.join(output_folder, 'y_labels.npy'), y)
-
 # Define the root folder containing subfolders with video files
 root_folder = "D:/git/video_to_frame_augmentation/Kathakali dataset video/test"
-output_folder = "D:/git/output_folder/"
+output_folder = "D:/git/output_folder"  # Output folder for CSV files
 
-
-# Initialize lists to store features and labels
-X = []  # Features
-y = []  # Labels
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
 
 # Iterate through all subfolders in the root folder
 for folder in os.listdir(root_folder):
     folder_path = os.path.join(root_folder, folder)
-    
+    output_subfolder = os.path.join(output_folder, folder)
+
+    # Create a subfolder in the output folder for each label
+    os.makedirs(output_subfolder, exist_ok=True)
+
     # Check if it's a directory (subfolder)
     if os.path.isdir(folder_path):
         # Iterate through video files in the subfolder
@@ -36,15 +34,14 @@ for folder in os.listdir(root_folder):
                 else:
                     print(f"Extracted {len(keyframes)} keyframes from {video_path}.")
 
-                # Extract features from keyframes using the vision transformer
-                frame_features = [extract_features_from_frame(frame) for frame in keyframes]
+                for idx, frame in enumerate(keyframes):
+                    # Extract features from keyframes using the vision transformer
+                    frame_features = extract_features_from_frame(frame)
 
-                # Append features and labels to the lists
-                X.append(frame_features)
-                y.append(folder)
+                    # Convert tensor to a list
+                    frame_features = frame_features.tolist()
 
-# Convert lists to NumPy arrays
-X = np.array(X)
-y = np.array(y)
-
-save_features_and_labels(X, y, output_folder)
+                    # Save features to a CSV file for each frame
+                    output_csv_file = os.path.join(output_subfolder, f"{video_file.split('.')[0]}_frame{idx}.csv")
+                    df = pd.DataFrame({'X': frame_features})
+                    df.to_csv(output_csv_file, index=False)
