6434a721be70109f9522d52b142b5d58b0d46145
jeslinpjames <jeslinpjames007@gmail.com>
2023-11-18 22:27:37 +0530

    Updated Keyframe Extraction to extract exactly n frames

--- key_frame_extraction.py
+++ key_frame_extraction.py
@@ -1,33 +1,48 @@
 import os
 import cv2
 
-def extract_keyframes(video_path, min_frame_difference=15000000):
+def extract_keyframes(video_path, n=120):
     vid = cv2.VideoCapture(video_path)
     keyframes = []  # Initialize a list to store keyframes
 
-    count = 0
-    flag = 0
-    x = 7  # Save every x frames
+    total_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    prev_frame = None
+    # If the video has fewer frames than n, duplicate the last frame continuously
+    if total_frames <= n:
+        print("Frames are less than ",n)
+        while len(keyframes) < n:
+            exist, frame = vid.read()
+            if not exist:
+                break
+            keyframes.append(frame)
 
-    while True:
-        exist, image = vid.read()
+        # Duplicate the last frame continuously until reaching n keyframes
+        while len(keyframes) < n:
+            keyframes.append(keyframes[-1])
 
-        if not exist:
-            break
+    else:
+        # Adjust min_frame_difference to extract exactly n keyframes if the video is larger than n
+        min_frame_difference = total_frames // n
+        flag = 0
+        prev_frame = None
+        print("Frames are greater than ",n)
 
-        if flag % x == 0:
-            keyframes.append(image)  # Add the frame to the list of keyframes
+        while len(keyframes) < n:
+            exist, image = vid.read()
 
-        if prev_frame is not None:
-            frame_difference = cv2.absdiff(prev_frame, image).sum()
+            if not exist:
+                break
 
-            if frame_difference > min_frame_difference:
-                keyframes.append(image)  # Add the frame to the list of keyframes
+            # Ensure both frames have the same shape before calculating the absolute difference
+            if prev_frame is not None and prev_frame.shape == image.shape:
+                frame_difference = cv2.absdiff(prev_frame, image).sum()
 
-        flag += 1
-        prev_frame = image
+                if flag == 0 or frame_difference > min_frame_difference:
+                    keyframes.append(image)  # Add the frame to the list of keyframes
+
+                flag += 1
+
+            prev_frame = image
 
     vid.release()
     return keyframes
