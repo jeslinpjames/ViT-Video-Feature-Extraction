79e9667fd0e0c40ca2e090670af5cf72a15c936d
jeslinpjames <jeslinpjames007@gmail.com>
2023-11-18 11:29:50 +0530

    Changed ViT model

--- image_feature_extraction_with_ViT.py
+++ image_feature_extraction_with_ViT.py
@@ -1,8 +1,9 @@
-from PIL import Image
 import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import timm
 import numpy as np
-from torchvision import transforms
-from pytorch_pretrained_vit import ViT
+from PIL import Image
 
 def extract_features_from_frame(frame, model=None):
     """
@@ -10,32 +11,38 @@ def extract_features_from_frame(frame, model=None):
 
     Args:
     frame (np.ndarray): The image frame as a NumPy array.
-    model (ViT): Pretrained ViT model (or None to create one).
+    model (nn.Module): Pretrained ViT model (or None to create one).
 
     Returns:
     torch.Tensor: Extracted features from the image.
     """
+    # Check if a GPU is available and move the model to the GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Load the ViT model if not provided
     if model is None:
-        model = ViT('B_16_imagenet1k', pretrained=True)
+        model = timm.create_model('vit_base_patch16_224', pretrained=True)
+        model = model.to(device)
         model.eval()
 
     # Convert the NumPy array to a PIL image
     img = Image.fromarray(frame)
 
+    # Check the number of channels
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
     # Resize the image to match the desired input size and preprocess it
     img = transforms.Compose([
-        transforms.Resize((384, 384)),  # You can change the size here
+        transforms.Resize((224, 224)),  # Adjust the size accordingly
         transforms.ToTensor(),
         transforms.Normalize(0.5, 0.5),
-    ])(img).unsqueeze(0)
+    ])(img).unsqueeze(0).to(device)
 
     # Use the model to extract features
     with torch.no_grad():
         features = model(img)
 
-    return features
-
+    return features.cpu()  # Move the features back to the CPU for further processing if needed
 
 
 if __name__ == "__main__":
