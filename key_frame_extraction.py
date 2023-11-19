c62e9a2fdc3af50c36f815a977f05b5307559c8c
jeslinpjames <jeslinpjames007@gmail.com>
2023-11-19 13:50:52 +0530

    Updated display and keyframe extraction function

--- key_frame_extraction.py
+++ key_frame_extraction.py
@@ -1,61 +1,48 @@
 import os
 import cv2
-
 def extract_keyframes(video_path, n=120):
     vid = cv2.VideoCapture(video_path)
     keyframes = []  # Initialize a list to store keyframes
 
     total_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # If the video has fewer frames than n, duplicate the last frame continuously
+    # If the video has fewer frames than n, duplicate frames to achieve the desired count
     if total_frames <= n:
-        print("Frames are less than ",n)
+        print("Frames are less than ", n)
         while len(keyframes) < n:
             exist, frame = vid.read()
             if not exist:
                 break
             keyframes.append(frame)
 
-        # Duplicate the last frame continuously until reaching n keyframes
-        while len(keyframes) < n:
-            keyframes.append(keyframes[-1])
-
     else:
-        # Adjust min_frame_difference to extract exactly n keyframes if the video is larger than n
-        min_frame_difference = total_frames // n
-        flag = 0
-        prev_frame = None
-        print("Frames are greater than ",n)
-
-        while len(keyframes) < n:
-            exist, image = vid.read()
-
+        # Extract exactly n keyframes if the video is larger than n
+        print("Frames are greater than ", n)
+        frame_indices = [int(i * total_frames / n) for i in range(n)]
+        for frame_index in frame_indices:
+            vid.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            exist, frame = vid.read()
             if not exist:
                 break
-
-            # Ensure both frames have the same shape before calculating the absolute difference
-            if prev_frame is not None and prev_frame.shape == image.shape:
-                frame_difference = cv2.absdiff(prev_frame, image).sum()
-
-                if flag == 0 or frame_difference > min_frame_difference:
-                    keyframes.append(image)  # Add the frame to the list of keyframes
-
-                flag += 1
-
-            prev_frame = image
+            keyframes.append(frame)
 
     vid.release()
     return keyframes
 
+def display_keyframes(keyframes, delay=50):
+    if not keyframes:
+        print("No keyframes to display.")
+        return
 
-def display_keyframes(keyframes):
-    for i, keyframe in enumerate(keyframes):
-        cv2.imshow(f"Keyframe {i + 1}", keyframe)
-        cv2.waitKey(0)  # Wait until a key is pressed
-        cv2.destroyAllWindows()
+    for frame in keyframes:
+        cv2.imshow("Keyframe Montage", frame)
+        cv2.waitKey(delay)  # Adjust the delay (in milliseconds) as needed
+
+    cv2.destroyAllWindows()
+
 
 def main():
-    video_path = "D:/git/video_to_frame_augmentation/Kathakali dataset video/Adbutham/Adbutham 1.mp4"  # Replace with the actual video path
+    video_path = "D:/git/video_to_frame_augmentation/Kathakali dataset video/Shantham/Shantham 2.mp4"
     keyframes = extract_keyframes(video_path)
 
     if not keyframes:
@@ -66,7 +53,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
