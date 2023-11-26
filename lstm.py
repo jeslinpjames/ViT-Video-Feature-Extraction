ec7dac07d0519d3ce3471874a2eb460339e71624
jeslinpjames <jeslinpjames007@gmail.com>
2023-11-26 16:28:45 +0530

    Fixed LSTM model

--- lstm.py
+++ lstm.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 from image_feature_extraction_with_ViT import extract_features_from_frame
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
@@ -14,45 +13,55 @@ import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 # Load the data from the CSV files in the folder
 folder_path = "D:/git/output_folder"
+print("Loading data from CSV files...")
 csv_files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
 
 data = []
 labels = []
 
-max_rows = 1003
-
 for csv_file in csv_files:
+    # Extract the class label from the file name
+    class_label = csv_file.split(" ")[0]
+    
+    # Read CSV file without shuffling
     df = pd.read_csv(os.path.join(folder_path, csv_file))
-    df = shuffle(df)  # Shuffle the rows
-    df = df.head(max_rows)  
+    
+    # Extract data and labels
     data.append(df.to_numpy())
-    labels.extend([csv_file.split(".")[0]] * len(df))
-
+    labels.extend([class_label])
+    # print(class_label)
+# Set random seeds
 tf.random.set_seed(42)
 random.seed(42)
 np.random.seed(42)
+
 # Extract the features and labels for each class
-X = np.vstack(data)
+X = data
 label_encoder = LabelEncoder()
 y = to_categorical(label_encoder.fit_transform(labels))  # One-hot encode labels
-with open ("label_encoder.pkl", 'wb') as f:
+with open("label_encoder.pkl", 'wb') as f:
     pickle.dump(label_encoder, f)
 
-# Combine the data and labels into a single dataset
-X_combined, y_combined = shuffle(X, y, random_state=42)
+# Combine the data and labels into a single dataset without shuffling
+X_combined, y_combined = np.array(X),np.array(y)
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_combined, y_combined, test_size=0.2, random_state=42)
-
+print(y_train)
+print(np.shape(y_train))
+print(np.shape(X_train))
+'''
 # Reshape the training and testing data to match the input shape of the LSTM model
 X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
 X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+'''
 
+print("Loaded data from csv files.")
+# print(X_train.shape)
 model = Sequential()
-model.add(LSTM(100, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(LSTM(100, return_sequences=True, input_shape=(120,1000)))
 model.add(LSTM(50, return_sequences=True, activation='relu'))
 model.add(LSTM(20, return_sequences=True, activation='relu'))
 model.add(LSTM(10, activation='relu'))
@@ -62,7 +71,7 @@ model.add(Dense(9, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the on the training set
-history = model.fit(X_train, y_train, epochs=25, batch_size=16, verbose=2)
+history = model.fit(X_train, y_train, epochs=25, batch_size=13, verbose=2)
 
 # Evaluate the model on the testing set
 loss, accuracy = model.evaluate(X_test, y_test)
